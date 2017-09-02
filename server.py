8e0267dc47d23427ae0d4daca2d101d182be5ddd
sven van straalen <ssgvanstraalen@gmail.com>
2017-09-02 14:52:21 +0200

    changed gui

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import time
 import datetime
 import threading
 
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, send_from_directory, jsonify
 from persistor import DataPersistor
 from theWeather import TheWeatherMan
 app = Flask(__name__)
